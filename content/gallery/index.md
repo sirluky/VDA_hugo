2b419879c054455d9a7fb7536f03386751c8ce8e
origin <lkov@post.cz>
2019-06-01 22:29:55 +0200

    presunuti me napodobeniny vda v Tailwind.CSS, na upravu pro Hugo

---
+++ content/gallery/index.md
@@ -0,0 +1,5 @@
+---
+title: 'Galerie - NEZOBRAZOVAT !!!'
+images: ['/images/rhl.jpg', '/images/gatsbystart.gif']
+desc: ['Rohlik z Babisovy zahradky', 'Ukazka cooool sablony']
+---
---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+console.log('hello world');
+let slCount = 0;
+const slsh = document.querySelector('#slideshow');
+const sltext = document.querySelector('#phototext');
+const slDots = document.querySelectorAll('.gallery-dots div');
+let sltextA = [];
+document.querySelectorAll('#slideshow .photo').forEach(p => sltextA.push(p.getAttribute('popis')));
+
+// let sltextA = [
+//   'Úspěchy našich studentů',
+//   'Česká Třebová má nové Centrum informačních technologií a Elektroniky',
+//   ' Dny otevřených dveří na VOŠ a SŠT Česká Třebová',
+// ];
+let prevSlIndex = 0;
+slsh.style.left = `-${slCount}00%`;
+sltext.innerText = sltextA[slCount];
+slDots[slCount].setAttribute('aktivni', '');
+
+setInterval(e => {
+  slCount = (slCount + 1) % slDots.length;
+
+  slsh.style.left = `-${slCount}00%`;
+  sltext.innerText = sltextA[slCount];
+  slDots[prevSlIndex].removeAttribute('aktivni');
+  slDots[slCount].setAttribute('aktivni', '');
+  prevSlIndex = slCount;
+}, 3500);
