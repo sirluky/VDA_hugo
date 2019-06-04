768f396abf5164112e97efbba2d3095e6754fa93
origin <lkov@post.cz>
2019-06-04 13:24:14 +0200

    dot click

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,11 @@
 console.log('hello world');
 let slCount = 0;
+let prevSlIndex = 0;
+
 const slsh = document.querySelector('#slideshow');
 const sltext = document.querySelector('#phototext');
 const slDots = document.querySelectorAll('.gallery-dots div');
+slDots.forEach(e => (e.onclick = changePhoto));
 let sltextA = [];
 document.querySelectorAll('#slideshow .photo').forEach(p => sltextA.push(p.getAttribute('popis')));
 
@@ -11,7 +14,15 @@ document.querySelectorAll('#slideshow .photo').forEach(p => sltextA.push(p.getAt
 //   'Česká Třebová má nové Centrum informačních technologií a Elektroniky',
 //   ' Dny otevřených dveří na VOŠ a SŠT Česká Třebová',
 // ];
-let prevSlIndex = 0;
+function changePhoto(e) {
+  slCount = e.target.getAttribute('index');
+  slsh.style.left = `-${slCount}00%`;
+  sltext.innerText = sltextA[slCount];
+  slDots[prevSlIndex].removeAttribute('aktivni');
+  slDots[slCount].setAttribute('aktivni', '');
+  prevSlIndex = slCount;
+}
+
 slsh.style.left = `-${slCount}00%`;
 sltext.innerText = sltextA[slCount];
 slDots[slCount].setAttribute('aktivni', '');
